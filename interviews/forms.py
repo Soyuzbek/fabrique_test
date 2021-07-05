067ea4bee5e6b2fb7728ae44906fd797d5f24b2a
soyuzbek <soyuzbek196.kg@gmail.com>
2021-07-05 22:52:16 +0600

    required 2 or more options when kind=choice|multiple

--- interviews/forms.py
+++ interviews/forms.py
@@ -20,4 +20,6 @@ class QuestionForm(forms.ModelForm):
         else:
             if not self.cleaned_data['options']:
                 self.add_error('options', 'Options is required when choice or multiple choice selected')
+            elif len(self.cleaned_data['options']) < 2:
+                self.add_error('options', 'Either add more options or choose kind text')
         return self.cleaned_data
